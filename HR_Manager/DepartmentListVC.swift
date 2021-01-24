acb2848992f89dd0ebb83df2d713bb94982b05ff
pazrvien <yjaewoong@naver.com>
2021-01-24 16:07:28 +0900

    sqlite 데이터베이스 테이블 생성 (FMDB) - department - employee

---
+++ HR_Manager/DepartmentListVC.swift
@@ -0,0 +1,12 @@
+//
+//  DepartmentListVC.swift
+//  HR_Manager
+//
+//  Created by 윤재웅 on 2021/01/24.
+//
+
+import UIKit
+
+class DepartmentListVC: UITableViewController {
+    
+}
---
+++ HR_Manager/EmployeeListVC.swift
@@ -0,0 +1,12 @@
+//
+//  EmployeeListVC.swift
+//  HR_Manager
+//
+//  Created by 윤재웅 on 2021/01/24.
+//
+
+import UIKit
+
+class EmployeeListVC: UITableViewController {
+    
+}
